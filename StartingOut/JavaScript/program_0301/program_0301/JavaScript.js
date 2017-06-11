43e26966dc25971c1a8ee7e2d234de6fc4bcd29f
ge01 <gabriel.escobedo01@utrgv.edu>
2017-06-10 23:20:56 -0500

    061017

--- StartingOut/JavaScript/program_0301/program_0301/JavaScript.js
+++
@@ -1,9 +0,0 @@
-﻿// This program demonstrates a function.
-// First, we define a function named message.
-function message() {
-    console.log("I am Arthur,");
-    console.log("King of the Britons.")
-}
-
-// Call the message function.
-message();
