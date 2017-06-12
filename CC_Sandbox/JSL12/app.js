2fef22aefbeb8e4834f01d49ba08ed3870877407
ge01 <gabriel.escobedo01@utrgv.edu>
2017-06-11 19:16:17 -0500

    CC - return a string passed in as an argument, whitespaces removed

--- CC_Sandbox/JSL12/app.js
+++ CC_Sandbox/JSL12/app.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+// Using the function given, return a string passed in as an argument, but with
+// all excess whitespaces removed.
+function noWhitespace(a) {
+
+}
+
+var str = "         abc        de fog         ";
+console.log(str);
+console.log(str.trim());
+console.log(str.replace(/\s+/g, ''));
