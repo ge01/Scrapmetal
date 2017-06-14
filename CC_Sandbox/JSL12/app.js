7cb3ebf339ac2a69d7e5677059a33c2aa5ef297e
ge01 <gabriel.escobedo01@utrgv.edu>
2017-06-13 21:42:21 -0500

    firstName, lastName

--- CC_Sandbox/JSL12/app.js
+++ CC_Sandbox/JSL12/app.js
@@ -1,10 +1,33 @@
 // Using the function given, return a string passed in as an argument, but with
 // all excess whitespaces removed.
-function noWhitespace(a) {
+// function noWhitespace(a) {
+//
+// }
+//
+// var str = "         abc        de fog         ";
+// console.log(str);
+// console.log(str.trim());
+// console.log(str.replace(/\s+/g, ''));
 
+// Inside the provided function, using a string literal template, return a string
+// where the parameters (firstName, lastName) are concatenated in a way as to provide
+// the following output if "Tom" and "Riddle" were passed in -
+// "Hello, Tom Riddle; nice to meet you.".
+
+// function literal(firstName, lastName) {
+//   let greeting1 = '"Hello,';
+//   let greeting2 = '; nice to meet you."';
+//   return `${greeting1} ${firstName} ${lastName}${greeting2}`;
+// }
+//
+// function literal(firstName, lastName) {
+//   return '"Hello, ' + firstName + " " + lastName + '; nice to meet you."';
+// }
+
+function literal(firstName, lastName) {
+  return `"Hello, ${firstName} ${lastName}; nice to meet you."`;
 }
 
-var str = "         abc        de fog         ";
-console.log(str);
-console.log(str.trim());
-console.log(str.replace(/\s+/g, ''));
+
+
+console.log(literal("Tom", "Riddle"));
