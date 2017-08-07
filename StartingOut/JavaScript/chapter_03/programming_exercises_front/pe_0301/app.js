9063a459f574e526caa4434face79b0caa30ad3d
ge01 <gabriel.escobedo01@utrgv.edu>
2017-08-07 00:39:21 -0500

    conversion to display working

--- StartingOut/JavaScript/chapter_03/programming_exercises_front/pe_0301/app.js
+++ StartingOut/JavaScript/chapter_03/programming_exercises_front/pe_0301/app.js
@@ -1,17 +1,19 @@
 var c1Button = document.getElementById("c1");
 var c2Button = document.getElementById("c2");
-
 var kmDisplay = document.querySelector("#kmDisplay");
 var miDisplay = document.querySelector("#miDisplay");
-
-var kMeter = 0;
+var kMeters = 0;
 var mile = 0;
 
 console.log(kmDisplay);
 console.log(c1);
 
 c1Button.addEventListener("click", function(){
-  kMeter++;
-  console.log(kMeter);
-  kmDisplay.textContent = kMeter;
+  kMeters++;
+  console.log(kMeters);
+  kmDisplay.textContent = kMeters;
+
+  miles = kMeters * 0.6214;
+
+  miDisplay.textContent = miles.toFixed(2);
 });
