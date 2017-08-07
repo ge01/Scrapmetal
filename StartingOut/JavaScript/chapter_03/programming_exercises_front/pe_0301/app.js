31d43c5355ef0ec93c3a023f69f6f4b947af3769
ge01 <gabriel.escobedo01@utrgv.edu>
2017-08-07 00:28:43 -0500

    display is set

--- StartingOut/JavaScript/chapter_03/programming_exercises_front/pe_0301/app.js
+++ StartingOut/JavaScript/chapter_03/programming_exercises_front/pe_0301/app.js
@@ -1,1 +1,17 @@
-alert("connected");
+var c1Button = document.getElementById("c1");
+var c2Button = document.getElementById("c2");
+
+var kmDisplay = document.querySelector("#kmDisplay");
+var miDisplay = document.querySelector("#miDisplay");
+
+var kMeter = 0;
+var mile = 0;
+
+console.log(kmDisplay);
+console.log(c1);
+
+c1Button.addEventListener("click", function(){
+  kMeter++;
+  console.log(kMeter);
+  kmDisplay.textContent = kMeter;
+});
