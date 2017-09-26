233fc05180a28560492ca46874903489af924c58
ge01 <gabriel.escobedo01@utrgv.edu>
2017-09-25 22:57:32 -0500

    another simple Java program

---
+++ 2017/StartingOut/Java/Chapter_02/CodeListing_0202/src/cl_0202.java
@@ -0,0 +1,7 @@
+// This is another simple Java program.
+public class cl_0202 {
+    public static void main(String[] args){
+        System.out.println("Programming is great fun!");
+        System.out.println("I can't get enough of it!");
+    }
+}
