a586e69622bc3bd54ab6f338d5acb7c47801e0ce
ge01 <gabriel.escobedo01@utrgv.edu>
2017-09-26 02:17:30 -0500

    Java - An unruly printing program

---
+++ 2017/StartingOut/Java/Chapter_02/CodeListing_0204/src/cl_0204.java
@@ -0,0 +1,11 @@
+// An unruly printing program
+public class cl_0204
+{
+    public static void main(String[] args)
+    {
+        System.out.print("These are our top seller:");
+        System.out.print("Computer games");
+        System.out.print("Coffee");
+        System.out.println("Aspirin");
+    }
+}
