d40a4c455624cddeae6028041ba772645c4e5462
ge01 <gabriel.escobedo01@utrgv.edu>
2017-10-24 06:39:50 -0500

    route params

--- 2017/03_WebDevBoot/FirstExpressApp/app.js
+++ 2017/03_WebDevBoot/FirstExpressApp/app.js
@@ -16,6 +16,20 @@ app.get("/dog", function(req, res) {
   res.send("MEOW!");
 });
 
+app.get("/r/:subredditName", function(req, res) {
+  var subreddit = req.params.subredditName;
+  res.send("WELCOME TO THE " + subreddit.toUpperCase() + " SUBREDDIT");
+});
+
+app.get("/r/:subredditName/comments/:id/:title/", function(req, res) {
+  console.log(req.params);
+  res.send("WELCOME TO THE COMMENTS PAGE!");
+});
+
+app.get("*", function(req, res) {
+  res.send("YOU ARE A STAR!!!");
+});
+
 app.listen(3000, function() {
   console.log("Server has stared!!!");
 });
