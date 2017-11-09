01c5965665db7cf73cd793acac62a3e870abba4b
ge01 <gabriel.escobedo01@utrgv.edu>
2017-11-09 01:37:52 -0600

    USB_Legacy_Disabled.txt is read

---
+++ 2017/05_Comparision/02_CompareFiles/02_CompareFiles/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace _02_CompareFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("USB_Legacy_Disabled.txt");
+
+            string line = "";
+            int counter = 0;
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                counter++;
+
+                Console.WriteLine("{0}:{1}", counter, line);
+            }
+
+            Console.WriteLine("we have {0} names in the txt file", counter);
+            Console.ReadLine();
+
+        }
+    }
+}
