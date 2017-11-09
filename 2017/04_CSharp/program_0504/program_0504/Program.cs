f13040d8f0b2ac386cb5d48a796604a405bb6514
ge01 <gabriel.escobedo01@utrgv.edu>
2017-11-09 00:19:15 -0600

    using streamwriter

---
+++ 2017/04_CSharp/program_0504/program_0504/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace program_0504
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare a StreamWriter variable.
+            StreamWriter outputFile;
+
+            // Create a file and get a StreamWriter object
+            outputFile = File.CreateText("Numbers.txt");
+
+            // Write the numbers 1 through 10; count++
+            for (int count = 1; count <= 10; count++)
+            {
+                outputFile.WriteLine(count);
+            }
+
+            // Close the file.
+            outputFile.Close();
+
+
+
+        }
+    }
+}
---
+++ 2017/04_CSharp/program_0504/program_0504/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("program_0504")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("program_0504")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("431c503d-0dd2-4bd4-b603-22d8a7ecb960")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
