71537abaab3b851946c3a2d81921db01fac8f24c
ge01 <gabriel.escobedo01@utrgv.edu>
2017-11-08 23:31:53 -0600

    C# tutorial - how to read from a txt file

---
+++ 2017/05_Comparision/01_ConsoleApplication9/01_ConsoleApplication9/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace _01_ConsoleApplication9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("TextFile1.txt");
+
+            string line = "";
+            int counter = 0;
+
+            while ((line = sr.ReadLine())!=null)
+            {
+                counter++;
+
+                Console.WriteLine("{0}:{1}", counter, line);                
+            }
+
+            Console.WriteLine("we have {0} names in the txt file", counter);
+            Console.ReadLine();
+        }
+    }
+}
