b18774dc49036b55c9c474907644ab7bd2ff96a9
ge01 <gabriel.escobedo01@utrgv.edu>
2017-11-09 00:32:57 -0600

    wirite out

---
+++ 2017/04_CSharp/WriteOut/WriteOut/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WriteOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String firstName;
+            String myOutLine;
+            Console.Write("Enter your first name:");
+            firstName = Convert.ToString(Console.ReadLine());
+            string path = @"file.txt";
+            myOutLine = firstName;
+
+            if (!File.Exists (path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine(myOutLine);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
