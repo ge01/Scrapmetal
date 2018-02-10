66e998fff35b2c6a1a8c3a42bd54732f4de49bdc
ge01 <gabriel.escobedo01@utrgv.edu>
2018-02-09 23:09:48 -0600

    Summation with for

---
+++ 2018/01_C/Deitel/Chapter_04/Fig_0405/Fig_0405/fig_0405.c
@@ -0,0 +1,18 @@
+// Fig. 4.5: fig_0405.c
+// Summation with for.
+#include <stdio.h>
+
+// function main begins program execution
+int main(void)
+{
+	unsigned int sum = 0; // initialize sum
+	unsigned int number; // number to be added to sum
+
+	for (number = 2; number <= 100; number += 2) {
+		sum += number; // add number to sum
+		printf("%u ", sum);
+	} // end for
+
+	printf("\n");
+	printf("Sum is %u\n", sum); // output sum
+} // end function main
